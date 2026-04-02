a8e083aa595cbca8338e002ecbffd30b067b7f62
output055 <asamoahclement055@gmail.com>
2026-04-02 07:17:23 +0100

    before the buckup plan

---
+++ app/Console/Commands/SyncToCloudDatabase.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Attributes\Signature;
+use Illuminate\Console\Command;
+
+#[Signature('app:sync-to-cloud-database')]
+#[Description('Command description')]
+class SyncToCloudDatabase extends Command
+{
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
